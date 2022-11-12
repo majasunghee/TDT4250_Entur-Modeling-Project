8434fe61b7c53a8b2f4d78c2e9aed14e892e95ed
majasunghee <majasonghee@gmail.com>
2022-11-12 14:27:56 +0100

    updated data content

--- tdt4250.entur.resource/src/fetchData/Fetch.java
+++ tdt4250.entur.resource/src/fetchData/Fetch.java
@@ -1,5 +1,21 @@
 package fetchData;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.emfjson.jackson.resource.JsonResourceFactory;
 public class Fetch {
 
+	Resource resource = resourceSet.createResource
+			  (URI.createFileURI("/tdt4250.entur.resource/src/data/SpecificLineData.json"));
+
+			resource.load(null);
 }
+
