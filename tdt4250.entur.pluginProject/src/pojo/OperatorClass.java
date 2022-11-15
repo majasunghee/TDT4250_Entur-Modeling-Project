cbc87c5d4e0fb6013df537a7152b4c35f2d201df
majasunghee <majasonghee@gmail.com>
2022-11-15 15:59:42 +0100

    Mapped OperatorsData.json to pojo

---
+++ tdt4250.entur.pluginProject/src/pojo/OperatorClass.java
@@ -0,0 +1,61 @@
+package pojo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.fasterxml.jackson.databind.JsonNode;
+
+public class OperatorClass {
+	
+	public String id;
+	public String name;
+	public String number;
+	public String url; 
+	public JsonNode lines;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	public JsonNode getLines() {
+		return lines;
+	}
+	public void setLines(JsonNode jsonNode) {
+		this.lines = jsonNode;
+	}
+
+	@Override
+	public String toString(){
+		StringBuilder sb = new StringBuilder();
+		sb.append("***** Operator *****\n");
+		sb.append("ID = "+getId()+"\n");
+		sb.append("Name = "+getName()+"\n");
+		sb.append("Phone = "+getNumber()+"\n");
+		sb.append("Url = "+getUrl()+"\n");
+		sb.append("Lines = "+getLines()+"\n");
+		return sb.toString();
+	}
+}
---
+++ tdt4250.entur.pluginProject/src/tdt4250/entur/pluginProject/Mapper.java
@@ -0,0 +1,72 @@
+package tdt4250.entur.pluginProject;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emfcloud.jackson.resource.JsonResourceFactory;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import pojo.OperatorClass;
+
+public class Mapper {
+	
+	private static String jsonpath1 = "../tdt4250.entur.pluginProject/src/data/OperatorsData.json";
+	private static String jsonpath2 = "../tdt4250.entur.pluginProject/src/data/LinesData.json";
+	private static String jsonpath3 = "../tdt4250.entur.pluginProject/src/data/SpecificLineData.json";
+	
+	
+	public static void main(String[] args) throws IOException {
+		ResourceSet resourceSet = new ResourceSetImpl();
+		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("json", new JsonResourceFactory());
+
+		//Read from json file and convert content to json string 
+		Resource resourceTest = resourceSet.createResource(URI.createFileURI(jsonpath1));
+			resourceTest.load(null);
+//			System.out.println("Halla");
+			
+		
+		
+		//Create ObjectMapper instance
+		ObjectMapper objmapper = new ObjectMapper();
+
+		
+		//Create new instances of operators
+		JsonNode treeNode = objmapper.readTree(Paths.get(jsonpath1).toFile());
+		treeNode=treeNode.findValue("operators");
+		OperatorClass operator = new OperatorClass();
+		for (Iterator<JsonNode> it = treeNode.elements(); it.hasNext(); ) {
+			ArrayList<JsonNode> listOfLines = new ArrayList<JsonNode>();
+		   JsonNode node = it.next();
+		   //System.out.println(node);
+		   operator.setId(node.findValue("id").textValue());
+		   operator.setName(node.findValue("name").textValue());
+		   operator.setNumber(node.findValue("phone").textValue());
+		   operator.setUrl(node.findValue("url").textValue());
+		   operator.setLines(node.get("lines"));
+		   for (JsonNode element: node.get("lines")) {
+			   listOfLines.add(element);
+		   }
+		   //operator.setLines(node.findValue("lines").textValue());
+		   System.out.println(operator);  
+		}
+		
+		
+		//convert json string to object
+//		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+	
+
+		
+
+		
+	}
+
+}
+
